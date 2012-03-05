b9cf402e113f4390a832d7a9927abac59d3e3f42
Tom Adams <tom@holizz.com>
2012-03-05 16:37:26 -0500

    Working

--- lib/wpdoc.rb
+++ lib/wpdoc.rb
@@ -8,15 +8,3 @@ end
 
 require "wpdoc/generator/shtml"
 require "wpdoc/c_parser_fix"
-
-unless defined? SDOC_FIXED_RDOC_OPTIONS
-  SDOC_FIXED_RDOC_OPTIONS = 1
-  class RDoc::Options
-    alias_method :rdoc_initialize, :initialize
-  
-    def initialize
-      rdoc_initialize
-      @generator = RDoc::Generator::SHtml
-    end
-  end
-end
